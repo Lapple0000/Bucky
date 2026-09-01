742b5eca0b3823c12722e73f244537db794f3379
Lapple0000 <cameron.gonzalez2010@gmail.com>
2026-09-01 13:09:02 -0500

    Add files via upload

--- shotgun_possibilities.py
+++ shotgun_possibilities.py
@@ -2,7 +2,29 @@ live = 'Live'
 blank = 'Blank'
 Live = live
 Blank = blank
+b = blank
+l = live
 
-possibilities = {'1l1b': ['Live, Blank'],
-                 '2l1b': ['Live', 'Live', 'Blank'],
-                 '1l2b': [live, Blank, blank]}
+shell_2 = {"pos_1": [l, b],
+           'pos_2': [b, l]}
+
+shell_3 = {"1l2b":
+           {'pos_1': [l, b, b],
+            'pos_2': [b, l, b],
+            'pos_3': [b, b, l]},
+           "2l1b":
+           {'pos_1': [l, l, b],
+            'pos_2': [l, b, l],
+            'pos_3': [b, l, l]}}
+
+shell_4 = {"1l3b":
+           {'pos_1': [l, b, b, b],
+            'pos_2': [b, l, b, b],
+            'pos_3': [b, b, l, b],
+            'pos_4': [b, b, b, l]},
+           "2l2b": []}
+
+shell_5 = {}
+shell_6 = {}
+shell_7 = {}
+shell_8 = {} 
